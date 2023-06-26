ea32dc2cb78bfc62b53f802046bcc984463316a2
Bilal Ali <bilal.ali@bluejeans.com>
2023-06-26 13:57:26 +0530

    Code Cleanup

---
+++ EmailInfo.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TokenizingTextBoxDemo
+{
+    public class EmailInfo
+    {
+        /// <summary>
+        /// Gets the formatted email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
+}
